82379bf9489cf1e9ae00a4cfdcb50ba2022720b6
tamanghawana0107 <hawanatamang@gmail.com>
2023-02-13 19:26:23 +0545

    Assignment

---
+++ Practicec/Hawana.java
@@ -0,0 +1,17 @@
+//Assignment
+public class Hawana{
+    public static void main(String[] args)
+    {
+        System.out.println("\n");
+        System.out.println("Name of the organization: Bajra Clothing Store");
+        System.out.print("\n");
+        System.out.println("Address:");
+        System.out.println("----State: Bagmati");
+        System.out.println("----City: Kathmandu");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println("\nContact Information:");
+        System.out.println("----Mobile Number: 9745332324");
+        System.out.println("----Email: bajraclothing@gmail.com");
+    }
+}
