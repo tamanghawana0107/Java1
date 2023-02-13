44c9da8fd4dfd33edb68d2bff218f1947ee75158
tamanghawana0107 <hawanatamang@gmail.com>
2023-02-13 11:35:42 +0545

    intial commit

---
+++ Practicec/Details.java
@@ -0,0 +1,7 @@
+public class Details{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: Hawana Tamang");
+        System.out.println("I study BIT in kist college.");
+    }
+}
---
+++ Practicec/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //code here
+        //String[] = datatype/array
+        //args = variable
+        System.out.println("hello world");
+    }
+}
