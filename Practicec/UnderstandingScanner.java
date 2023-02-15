456520db6d62c9123fc11f1d547f3b4ba465fddb
tamanghawana0107 <hawanatamang@gmail.com>
2023-02-15 11:42:47 +0545

    Updated

--- Practicec/UnderstandingScanner.java
+++ Practicec/UnderstandingScanner.java
@@ -6,6 +6,15 @@ public class UnderstandingScanner
         System.out.println("Enter your name:");
         Scanner obj = new Scanner(System.in);            //Scanner is a class , obj is a object
         String name = obj.nextLine();         // nextLine will wait for the user
+        
+        System.out.println("Enter your number:");
+        int num = obj.nextInt();
+        System.out.println("Your number is " + num*50);
+        
+        System.out.println("Is java fun :");
+        boolean fun = obj.nextBoolean();
+        System.out.println(fun);
+        
         // System.out.println("Hello world");
         // System.out.println("New code to be written");
         // System.out.println("Enter your name:");
