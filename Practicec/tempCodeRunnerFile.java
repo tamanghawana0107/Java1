a5c00d6113bdd0b2a49e9fb7bf2d57430fd30123
tamanghawana0107 <hawanatamang@gmail.com>
2023-02-23 11:17:36 +0545

    Added

---
+++ Practicec/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+        // String[] college = {"Kist College"};
+        // System.out.println("\n" +college[0]);
+        // String[] cars = {"kwid","BMW"};
+        // for(int i = 0; i<2;i++)
+        // {
+        //         System.out.println(cars[i]);
+        // }
+        // System.out.println("\nUsing for each loop");
+        // for( String i : cars)
+        // {
+        //     System.out.println(i);
+        // }  
