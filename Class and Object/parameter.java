73219efecb6bf7d45e211d0a5d1ce6965e7d59f1
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-02 20:38:51 +0545

    return code added

--- Class and Object/parameter.java
+++ Class and Object/parameter.java
@@ -5,13 +5,20 @@ public class parameter
     {
         System.out.println(fname + " Tamang");
     }
+    // multiple parameter code
     static void nextMethod(String fname , int age)
     {
         System.out.println(fname + " Tamang "+ " is " + age + " years old");
+    }
+    //return value method
+    static int retMethod(int x)
+    {
+        return 5 + x;
     }
     public static void main(String[] args)
     {
         myMethod("Hawana");
         nextMethod("Hawana",19);
+        System.out.println("Ans = " + retMethod(3));
     }
 }
