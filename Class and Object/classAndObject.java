3934f426dd1ff8aee6ac93b2f02f1c1d6bf116ae
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-02 20:28:07 +0545

    OOP

---
+++ Class and Object/classAndObject.java
@@ -0,0 +1,9 @@
+public class classAndObject{
+    public static void main(String[] args)
+    {
+        int width =10;       // attribute of class
+        classAndObject cO = new classAndObject();
+        System.out.println("Width = " + width);
+    }
+   
+}
---
+++ Class and Object/method.java
@@ -0,0 +1,10 @@
+public class method{
+    static void myCode()
+    {
+        System.out.println("Method calling");
+    }
+    public static void main(String[] args)
+    {
+        myCode();
+    }
+}
