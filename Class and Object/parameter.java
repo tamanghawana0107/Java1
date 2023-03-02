3e30029ff8d5b69f36acfd8ae396a86ad3b907d4
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-02 20:33:14 +0545

    parameter code added

---
+++ Class and Object/parameter.java
@@ -0,0 +1,12 @@
+// java method parameter
+public class parameter
+{
+    static void myMethod(String fname)
+    {
+        System.out.println(fname + " Tamang");
+    }
+    public static void main(String[] args)
+    {
+        myMethod("Hawana");
+    }
+}
