413520625ccc72b2e1cec10b75d69662b8a1e7c6
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-02 20:35:59 +0545

    multiple parameter code added

--- Class and Object/parameter.java
+++ Class and Object/parameter.java
@@ -5,8 +5,13 @@ public class parameter
     {
         System.out.println(fname + " Tamang");
     }
+    static void nextMethod(String fname , int age)
+    {
+        System.out.println(fname + " Tamang "+ " is " + age + " years old");
+    }
     public static void main(String[] args)
     {
         myMethod("Hawana");
+        nextMethod("Hawana",19);
     }
 }
