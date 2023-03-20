c70e398b3f8cf022a6171384bc688dab252b06db
tamanghawana0107 <hawanatamang@gmail.com>
2023-03-20 22:27:21 +0545

    permutation based array code added

--- Assignment/Assignment8.java
+++ Assignment/Assignment8.java
@@ -1,12 +1,27 @@
 //printing array
-
+import java.util.Scanner;
 public class Assignment8 {
     public static void main(String[] args)
     {
-        int[] num = {0,2,1,5,3,4};
-        int[] ans = new int[6];
-        System.out.println("The entered element is [0,2,1,5,3,4]");
-        
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter how many element you want to enter: ");
+        int n = input.nextInt();
+        int[] nums = new int[n];
+        int ans[] = new int[n];
+        System.out.println("Enter the element inside the array: ");
+        for(int a=0;a<n;a++)
+        {
+            nums[a] = input.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        System.out.println("The output of the given array is :");
+        for(int i=0; i<n;i++)
+        {
+            System.out.print(ans[i] + " ");
+        }
+        input.close();
     }
 }
